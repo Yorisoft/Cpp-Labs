1e79b19dae8389183db243e975becf29436a740a
Yelsin Sepulveda <yelsinsepulveda@gmail.com>
2021-03-29 23:35:06 -0400

    Renaming test file to include work 'test', update premake5 file

--- 284/Group_Project/BankAccounts/scripts/linux-build.sh
+++ 284/Group_Project/BankAccounts/scripts/linux-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd 284/Group_Project/BankAccounts/
-vendor/bin/premake/premake5 gmake2
-make config=deposit-fees-test
+vendor/bin/premake/premake5 gmake2 config=deposit-fees-test
+make 
 
 #nmake -f Bootstrap.mak windows
