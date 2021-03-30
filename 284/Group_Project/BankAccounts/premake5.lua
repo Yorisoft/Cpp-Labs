b106de8d5f844b9c5862a19a5d85870a6456d2c7
Yelsin Sepulveda <yelsinsepulveda@gmail.com>
2021-03-29 22:15:26 -0400

    Renaming test file to include work 'test', update premake5 file

--- 284/Group_Project/BankAccounts/premake5.lua
+++ 284/Group_Project/BankAccounts/premake5.lua
@@ -1,6 +1,5 @@
 -- premake5.lua
 workspace "testProject"
-   configurations { "Debug", "Release" }
 
 --[[ dialect { 
    ['C'] = 'C99', 
@@ -12,37 +11,23 @@ project "BankAccounts"
    kind "ConsoleApp"
    language "C++"
    targetdir "bin/%{cfg.buildcfg}"
-
    files { "**.h", "**.cpp" }
 
-   filter "configurations:Debug"
+   configurations { "deposit-fees-test", "Debug", "Release" }
+
+   filter {"configurations:Debug"}
       excludes "checking-account-deposit-fees.test.cpp"
       cppdialect "C++11"
       defines { "DEBUG" }
       symbols "On"
 
-   filter "configurations:Release"
+   filter {"configurations:Release"}
       excludes "checking-account-deposit-fees.test.cpp"
       cppdialect "C++11"
       defines { "NDEBUG" }
       optimize "On"
 
-
-project "checking-account-deposit-fees-test"
-   location "testProject"
-   kind "ConsoleApp"
-   language "C++"
-   targetdir "bin/%{cfg.buildcfg}"
-
-   files { "**.h", "**.cpp" }
-
-   filter "configurations:Debug"
-      excludes "main.cpp"
-      cppdialect "C++11"
-      defines { "DEBUG" }
-      symbols "On"
-
-   filter "configurations:Release"
+   filter {"configurations:deposit-fees-test"}
       excludes "main.cpp"
       cppdialect "C++11"
       defines { "NDEBUG" }
